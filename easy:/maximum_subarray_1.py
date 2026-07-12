3c415f2aea7e437c1c3a53506d0878411af88e0c
destinyalliah <destinyalliahh@gmail.com>
2026-07-12 22:18:44 +0100

    add subarray solution

---
+++ easy:/maximum_subarray_1.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        n = len(nums)
+        cur_sum = 0
+
+        for i in range(k):
+            cur_sum += nums[i]
+        max_avg = cur_sum / float(k)
+        
+        for i in range(k, n):
+            cur_sum += nums[i]
+            cur_sum -= nums[i-k]
+            avg = cur_sum / float(k)
+            max_avg = max(max_avg, avg)
+
+        return max_avg
